f54655630787d3e4c5073edf7c7fb9dc5111bb08
Gourav Ahlawat <cse210001019@gmail.com>
2023-12-05 16:17:18 +0530

    trying to render movies

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -13,43 +13,7 @@ export const tmdbApi = createApi({
         }),
         }),
         });
-    //     //* Get Genres
-    //     getGenres: builder.query({
-    //         query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
-    //     }),
-
-    //     //* Get Movie
-    //     getMovie: builder.query({
-    //         query: (id) => `/movie/${id}?api_key=${tmdbApiKey}&append_to_response=videos,credits`,
-    //     }),
-
-    //     //* Get user specific movie lists
-    //     getRecommendations: builder.query({
-    //         query: ({ movie_id, list }) => `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
-    //     }),
-
-    //     //* Get Actor's Detail
-    //     getActorDetail: builder.query({
-    //         query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
-    //     }),
-
-    //     //* Get Actor's Movies
-    //     getMoviesByActorId: builder.query({
-    //         query: (id, page) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
-    //     }),
-
-    //     //* Get favorite/watchlist movies
-    //     getList: builder.query({
-    //         query: ({ listName, accountId, sessionId, page }) => `/account/${accountId}/${listName}?api_key=${tmdbApiKey}&session_id=${sessionId}&page=${page}`,
-    //     }),
-    // }),
 
 export const {
     useGetMoviesQuery,
-    // useGetGenresQuery,
-    // useGetMovieQuery,
-    // useGetRecommendationsQuery,
-    // useGetActorDetailQuery,
-    // useGetMoviesByActorIdQuery,
-    // useGetListQuery,
 } = tmdbApi;
