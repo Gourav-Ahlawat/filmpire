8e41099cfc2a68aab96afd30ea38353003bb770e
Gourav Ahlawat <cse210001019@gmail.com>
2023-12-05 13:37:33 +0530

    trying to make api calls

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+
 
 const Movies = () => {
+  const {data} = useGetMoviesQuery()
   return (
     <div>Movies</div>
   )
--- src/components/styles.js
+++ src/components/styles.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@mui/styles';
 export default makeStyles(() => ({
   root: {
     display: 'flex',
+    flexDirection: 'column',
     height: '100%',
   },
   toolbar: {
@@ -12,6 +13,7 @@ export default makeStyles(() => ({
     flexGrow: 1,
     padding: '2rem',
     width: '100%',
+    marginLeft: '240px',
   },
   
 }));
