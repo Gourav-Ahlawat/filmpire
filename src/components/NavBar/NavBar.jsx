6bf09c1414d3b6c6b245202cf2c9f0f1dc77e2e7
Gourav Ahlawat <cse210001019@gmail.com>
2023-12-05 12:38:05 +0530

    menu button for sidebar fixed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
             color="inherit"
             edge="start"
             style={{ outline: 'none' }}
-            onClick={()=>{}}
+            onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
             className={classes.menuButton}
           >
             <Menu />
@@ -68,7 +68,7 @@ const NavBar = () => {
               variant="temporary"
               anchor="left"
               open={mobileOpen}
-              // onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
               classes={{ paper: classes.drawerPaper }}
               ModalProps={{ keepMounted: true }}
             >
